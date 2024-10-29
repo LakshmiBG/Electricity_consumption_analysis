36d03e6c2f0d9ab29ef9a70f12596fb2f62a2a59
Lakshmi BG <51742367+LakshmiBG@users.noreply.github.com>
2024-10-29 15:58:01 +0200

    Update Electricity_consumption_analysis.py

--- Electricity_consumption_analysis.py
+++ Electricity_consumption_analysis.py
@@ -28,32 +28,9 @@ df = df.rename(columns={"Energy (kWh)": "energy_kwh", "Price (cent/kWh)": "price
 df['bill_euro'] = df['energy_kwh']*df['price_kwh_cent']/100
 # print(df.head())
 
-# Calculated grouped values of daily, weekly or monthly consumption, bill, average price
-# and average temperature
-
-
-#freq = 'd', grouping over one day
-#freq = 'w', grouping over week
-#freq = 'm', grouping over month
-
-df_daily_avg = (df.groupby(pd.Grouper(key = 'time', freq = 'd'))[['energy_kwh','bill_euro','price_kwh_cent','Temperature']]).mean().reset_index()
-df_weekly_avg = (df.groupby(pd.Grouper(key = 'time', freq = 'w'))[['energy_kwh','bill_euro','price_kwh_cent','Temperature']].mean()).reset_index()
-df_monthly_avg = (df.groupby(pd.Grouper(key = 'time', freq = 'm'))[['energy_kwh','bill_euro','price_kwh_cent','Temperature']].mean()).reset_index()
-
-#df_monthly_avg.columns
-
-
-df_daily_avg['frequency'] = 'daily'
-df_weekly_avg['frequency'] = 'weekly'
-df_monthly_avg['frequency'] = 'monthly'
-
-df_daily_avg['datetime'] = df['time'].dt.date
-df_weekly_avg['datetime'] = df['time'].dt.date
-df_monthly_avg['datetime'] = df['time'].dt.date
-
-# get min date from data
-min_date = df_daily_avg['datetime'].min()
-max_date = df_daily_avg['datetime'].max()
+# Get minimum and maximum dates for setting date range in Streamlit
+min_date = df['time'].min().date()
+max_date = df['time'].max().date()
 
 d1 = st.date_input("Start time", datetime.date(df['DateTime'][0].year, df['DateTime'][0].month, df['DateTime'][0].day))
 d2 = st.date_input("End time", datetime.date(df['DateTime'][len(df)-1].year, df['DateTime'][len(df)-1].month, df['DateTime'][len(df)-1].day))
@@ -63,11 +40,27 @@ st.write("Showing range:", d1,' - ',d2)
 d1 = pd.to_datetime(d1)
 d2 = pd.to_datetime(d2)
 
-#And then selecting the period from data:
+
+# Filter the data based on the selected period
 df = df[df['DateTime']>d1]
 df = df[df['DateTime']<d2]
 
-print(min_date, max_date)
+# Calculated grouped values of daily, weekly or monthly consumption, bill, average price and average temperature
+#freq = 'd', grouping over one day
+#freq = 'w', grouping over week
+#freq = 'm', grouping over month
+
+df_daily_avg = (df.groupby(pd.Grouper(key = 'time', freq = 'd'))[['energy_kwh','bill_euro','price_kwh_cent','Temperature']]).mean().reset_index()
+df_weekly_avg = (df.groupby(pd.Grouper(key = 'time', freq = 'w'))[['energy_kwh','bill_euro','price_kwh_cent','Temperature']].mean()).reset_index()
+df_monthly_avg = (df.groupby(pd.Grouper(key = 'time', freq = 'm'))[['energy_kwh','bill_euro','price_kwh_cent','Temperature']].mean()).reset_index()
+
+df_daily_avg['frequency'] = 'daily'
+df_weekly_avg['frequency'] = 'weekly'
+df_monthly_avg['frequency'] = 'monthly'
+
+df_daily_avg['datetime'] = df['time'].dt.date
+df_weekly_avg['datetime'] = df['time'].dt.date
+df_monthly_avg['datetime'] = df['time'].dt.date
 
 #Start and end date input
 start_date = st.date_input(label='Start Date',format="YYYY/MM/DD", value=d1)
@@ -77,7 +70,6 @@ end_date = st.date_input(label='End Date',format="YYYY/MM/DD", value=d2)
 if start_date > end_date:
     st.error("Error: Start date must not be later than end date. Please select again.")
 else:
-    # Proceed with the rest of your logic only if the dates are valid
     st.write(f'Showing range:', start_date, end_date)
     st.write(f'Consumption statistics can be found at the end of the page')
 
