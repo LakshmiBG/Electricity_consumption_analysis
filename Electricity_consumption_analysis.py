63bdb09e4cf7932fd8846f9e9e5cf96e09c81092
Lakshmi BG <51742367+LakshmiBG@users.noreply.github.com>
2024-10-29 16:53:51 +0200

    Update Electricity_consumption_analysis.py

--- Electricity_consumption_analysis.py
+++ Electricity_consumption_analysis.py
@@ -34,67 +34,74 @@ st.write("Showing range:", d1, " - ", d2)
 d1 = pd.to_datetime(d1)
 d2 = pd.to_datetime(d2)
 
-# Filter the data based on the selected period
-df = df[(df['time'] >= d1) & (df['time'] <= d2)]
 
-# Calculate daily, weekly, and monthly averages after filtering
-df_daily_avg = df.groupby(pd.Grouper(key='time', freq='d'))[['energy_kwh', 'bill_euro', 'price_kwh_cent', 'Temperature']].mean().reset_index()
-df_weekly_avg = df.groupby(pd.Grouper(key='time', freq='w'))[['energy_kwh', 'bill_euro', 'price_kwh_cent', 'Temperature']].mean().reset_index()
-df_monthly_avg = df.groupby(pd.Grouper(key='time', freq='m'))[['energy_kwh', 'bill_euro', 'price_kwh_cent', 'Temperature']].mean().reset_index()
+#Validating the data range
+if d1 < min_date or d2 > max_date:
+    st.error(f'Please select dates withing the available range:' min_date, '-', max_date)
+elif d1 > d2:
+    st.error('Error: Start date must not be later than end date. Please select again.')
+else:
+    # Filter the data based on the selected period
+    df = df[(df['time'] >= d1) & (df['time'] <= d2)]
 
-# Add a frequency column
-df_daily_avg['frequency'] = 'daily'
-df_weekly_avg['frequency'] = 'weekly'
-df_monthly_avg['frequency'] = 'monthly'
+    # Calculate daily, weekly, and monthly averages after filtering
+    df_daily_avg = df.groupby(pd.Grouper(key='time', freq='d'))[['energy_kwh', 'bill_euro', 'price_kwh_cent', 'Temperature']].mean().reset_index()
+    df_weekly_avg = df.groupby(pd.Grouper(key='time', freq='w'))[['energy_kwh', 'bill_euro', 'price_kwh_cent', 'Temperature']].mean().reset_index()
+    df_monthly_avg = df.groupby(pd.Grouper(key='time', freq='m'))[['energy_kwh', 'bill_euro', 'price_kwh_cent', 'Temperature']].mean().reset_index()
 
-# Extract date from datetime
-df_daily_avg['datetime'] = df_daily_avg['time'].dt.date
-df_weekly_avg['datetime'] = df_weekly_avg['time'].dt.date
-df_monthly_avg['datetime'] = df_monthly_avg['time'].dt.date
+    # Add a frequency column
+    df_daily_avg['frequency'] = 'daily'
+    df_weekly_avg['frequency'] = 'weekly'
+    df_monthly_avg['frequency'] = 'monthly'
 
-# Grouping interval selection
-option = st.selectbox("Select an option:", ["Daily", "Weekly", "Monthly"])
+    # Extract date from datetime
+    df_daily_avg['datetime'] = df_daily_avg['time'].dt.date
+    df_weekly_avg['datetime'] = df_weekly_avg['time'].dt.date
+    df_monthly_avg['datetime'] = df_monthly_avg['time'].dt.date
 
-# Filter and display charts based on selected option
-if option == 'Daily':
-    df_filtered = df_daily_avg
-elif option == 'Weekly':
-    df_filtered = df_weekly_avg
-else:  # Monthly
-    df_filtered = df_monthly_avg
+    # Grouping interval selection
+    option = st.selectbox("Select an option:", ["Daily", "Weekly", "Monthly"])
 
-# Plot charts
-st.line_chart(data=df_filtered, x='time', y='energy_kwh', y_label='Energy consumption (kWh)', x_label='Time')
-st.line_chart(data=df_filtered, x='time', y='price_kwh_cent', y_label='Electricity price (cents)', x_label='Time')
-st.line_chart(data=df_filtered, x='time', y='bill_euro', y_label='Electricity bill (Euros)', x_label='Time')
-st.line_chart(data=df_filtered, x='time', y='Temperature', y_label='Temperature (°C)', x_label='Time')
+    # Filter and display charts based on selected option
+    if option == 'Daily':
+        df_filtered = df_daily_avg
+    elif option == 'Weekly':
+        df_filtered = df_weekly_avg
+    else:
+        df_filtered = df_monthly_avg
 
-# Display summary statistics for the selected period
-total_consumption = round(df_filtered['energy_kwh'].sum(), 2)
-total_bill = round(df_filtered['bill_euro'].sum(), 2)
-average_price = round(df_filtered['price_kwh_cent'].mean(), 2)
-average_paid_price = round((total_bill / total_consumption)*100, 2) if total_consumption else 0
+    # Plot charts
+    st.line_chart(data=df_filtered, x='time', y='energy_kwh', y_label='Energy consumption (kWh)', x_label='Time')
+    st.line_chart(data=df_filtered, x='time', y='price_kwh_cent', y_label='Electricity price (cents)', x_label='Time')
+    st.line_chart(data=df_filtered, x='time', y='bill_euro', y_label='Electricity bill (Euros)', x_label='Time')
+    st.line_chart(data=df_filtered, x='time', y='Temperature', y_label='Temperature (°C)', x_label='Time')
 
-#if not df_filtered['price_kwh_cent'].empty:
-    #highest_price_date = df_filtered.loc[df_filtered['price_kwh_cent'].idxmax()]['datetime']
-#else:
-    #highest_price_date = None
+    # Display summary statistics for the selected period
+    total_consumption = round(df_filtered['energy_kwh'].sum(), 2)
+    total_bill = round(df_filtered['bill_euro'].sum(), 2)
+    average_price = round(df_filtered['price_kwh_cent'].mean(), 2)
+    average_paid_price = round((total_bill / total_consumption)*100, 2) if total_consumption else 0
 
-#if not df_filtered['price_kwh_cent'].empty:
-    #lowest_price_date = df_filtered.loc[df_filtered['price_kwh_cent'].idxmin()]['datetime']
-#else:
-    #lowest_price_date = None
+    #if not df_filtered['price_kwh_cent'].empty:
+        #highest_price_date = df_filtered.loc[df_filtered['price_kwh_cent'].idxmax()]['datetime']
+    #else:
+        #highest_price_date = None
 
-
-st.write(f'Total consumption over the period', total_consumption, 'kWh')
-st.write(f'Total bill over the period:', total_bill, '€')
-st.write(f'Average price:', average_price, 'cents/kWh')
-st.write(f'Average paid price:', average_paid_price, 'cents/kWh')
-#st.write(f'Date with highest price:', highest_price_date)
-#st.write(f'Date with lowest price:', lowest_price_date)
+    #if not df_filtered['price_kwh_cent'].empty:
+        #lowest_price_date = df_filtered.loc[df_filtered['price_kwh_cent'].idxmin()]['datetime']
+    #else:
+        #lowest_price_date = None
 
 
-monthly_cost = df_filtered.groupby(df_filtered['time'].dt.month)['bill_euro'].sum().round(2)
-st.bar_chart(monthly_cost, y_label = 'Electricity bill (Euro)', x_label = 'Months')
+    st.write(f'Total consumption over the period', total_consumption, 'kWh')
+    st.write(f'Total bill over the period:', total_bill, '€')
+    st.write(f'Average price:', average_price, 'cents/kWh')
+    st.write(f'Average paid price:', average_paid_price, 'cents/kWh')
+    #st.write(f'Date with highest price:', highest_price_date)
+    #st.write(f'Date with lowest price:', lowest_price_date)
+
+
+    monthly_cost = df_filtered.groupby(df_filtered['time'].dt.month)['bill_euro'].sum().round(2)
+    st.bar_chart(monthly_cost, y_label = 'Electricity bill (Euro)', x_label = 'Months')
 
 
