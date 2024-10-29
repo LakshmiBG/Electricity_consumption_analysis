db44b02d31d4f1cb40d0b5ccc470115dc92a175e
Lakshmi BG <51742367+LakshmiBG@users.noreply.github.com>
2024-10-29 18:21:34 +0200

    Update Electricity_consumption_analysis.py

--- Electricity_consumption_analysis.py
+++ Electricity_consumption_analysis.py
@@ -99,9 +99,11 @@ else:
     st.write(f'Average paid price:', average_paid_price, 'cents/kWh')
     st.write(f'Date with highest price:', highest_price_date)
     st.write(f'Date with lowest price:', lowest_price_date)
+    st.write(f'')
 
-
+    
+    st.write(f'Monthly Distribution of Energy Cost)
     monthly_cost = df_filtered.groupby(df_filtered['time'].dt.month)['bill_euro'].sum().round(2)
-    st.bar_chart(monthly_cost, y_label = 'Electricity bill (Euro)', x_label = 'Months')
+    st.bar_chart(monthly_cost, y_label = 'Electricity bill (Euro)', x_label = 'Months (numbered)')
 
 
